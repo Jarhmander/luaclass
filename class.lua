e94b9012d391c938c56bcaf809cbf1f57aefc006
James-Adam Renquinha Henri <jarhmander@gmail.com>
2017-08-26 16:04:21 -0400

    Handling of class/instance names, partial support for MRO.

--- class.lua
+++ class.lua
@@ -1,37 +1,104 @@
+local class
+local coro = coroutine
+local yield = coro.yield
+
+local debug
+
+--[[!
+--  @brief Small helper: return (given) table with itself as metatable
+--]]
 local function self_meta(t)
     return setmetatable(t, t)
 end
 
-local class = self_meta {}
+--[[!
+--  @brief Implementation of `class.getattr`.
+--]]
+local function class_resolve_attr(inst_or_class, attr)
+    local resolver = coroutine.wrap(function ()
+        local cls = class.isclass(inst_or_class) and inst_or_class or inst_or_class.__class
+        for _, base in ipairs(cls.__mro) do
+            local ret = rawget(base, attr)
+            if ret ~= nil then return ret, base end
+        end
+        return nil
+    end)
+    local mem, cls = resolver()
+    if mem then return mem, cls, resolver else return nil end
+end
 
-class.object = self_meta {}
-
-class.object.__bases = {}
-
-local default_base = class.object
-
+--[[!
+--  @brief Internal; on a fresh new instance, calls the ctor if it exists.
+--]]
 local function class_init_instance(cls, instance, ...)
     local ctor = rawget(cls, "__ctor")
     if ctor then ctor(instance, ...) end
     return instance
 end
 
+--[[!
+--  @brief Implementation of `dtor` and `__gc`: destroy instance
+--]]
 local function class_destroy_instance(instance)
-    -- FIX multi call to dtor in MI
-    local visited = {}
-    local function recurse_dtor(class, instance)
-        if visited[class] then return end
-        visited[class] = true
-        local dtor = rawget(class, "__dtor")
-        if dtor then dtor(instance) end
-
-        for _, base in ipairs(instance.__bases) do
-            class_destroy_instance(base, instance)
-        end
+    local dtor, _, resolv = class_resolve_attr(instance, "__dtor")
+    while dtor do
+        dtor(instance)
+        dtor = resolv()
     end
-    return recurse_dtor(instance.__class, instance)
 end
 
+--[[!
+--  @brief Internal; try to get the classname
+--]]
+local function class_try_get_classname(cls)
+    local classname
+
+    local function fallback()
+        if not classname then
+            cls.__name = "anon" -- So tostring works.
+            local s = tostring(cls)
+            local hex = s:match("0x(%x+)")
+            classname = "_anon_" .. (hex or '???')
+        end
+        cls.__classname = classname
+    end
+
+    if not debug then
+        local ok, pkg = pcall(require, "debug")
+        if not ok then return fallback() end
+        debug = pkg
+    end
+
+    local dbg = debug.getinfo(3)
+    local filename = string.match(dbg.source, "@(.+)")
+    local f = filename and io.open(filename, "r")
+    if not f then return fallback() end
+
+    local line
+    for i = 1, dbg.currentline do
+        line = f:read()
+    end
+
+    classname = line:match "([%a_][%w_]*)%s*=%s*class%f[%s%p]"
+    return fallback()
+end
+
+class = self_meta {}
+
+local default_base = self_meta {}
+
+default_base.__bases = {}
+default_base.__mro = {default_base}
+default_base.dtor = class_destroy_instance
+
+--[[!
+--  @brief class `object`, the base class of all classes.
+--]]
+class.object = default_base
+
+--[[!
+--  @brief Internal; make new class with some defined contents
+--]]
 local function class_create()
     local cls = { __bases = default_base }
 
@@ -41,6 +108,7 @@ local function class_create()
         instance.__index = self
         instance.__gc = class_destroy_instance
         instance.__class = self
+        instance.__name = class.type(instance)
 
         self_meta(instance)
 
@@ -48,26 +116,52 @@ local function class_create()
     end
 
     function cls:__index(member)
-        -- TODO: use C3 linearization algorithm, and put in field __mro ?
-        for _, base in ipairs(self.__bases) do
-            local value = base[member]
-            if value then return value end
-        end
-        return nil
+        return class_resolve_attr(self, member)
     end
 
     return self_meta(cls)
 end
 
 --[[
--- class:
+-- class' metatable:
 --      __bases
+--      __mro
 --      __name
+--      __classname
 --
--- instance:
+-- instance's metatable:
 --      __class
+--      __name
 --      __index
 --      __gc
+--
+--]]
+
+--[[!
+--  @brief Creation of a new class
+--
+--  This constructor can be used as follows:
+--      [local] c = class {...}
+--      [local] c = class "Name" {...}
+--      [local] c = class (Base1 [, Base2 ...]) {...}
+--      [local] c = class "Name" (Base1 [, Base2 ...]) {...}
+--
+--  More succintly, the syntax is as follows:
+--      class [classname] [(bases...)] <attrlist>
+--
+--  In other words, you can omit the class name and/or the base classes specification but `attrlist`
+--  is required.
+--
+--  If `classname` is omitted, the name is deduced automatically (not reliable: uses the debug
+--  library, and requires the script to be available, and works only for simple cases). If it fails
+--  to get the name automatically, the deduced `classname` will be `_anon_HHH`, where HHH is a
+--  unique sequence of hex digits.
+--
+--  If `bases` is omitted, the class inherits from `class.object`.
+--
+--  `attrlist` is a possibly empty list of class attributes: regular functions, member functions and
+--  class members (variable shared accross all instances of a class, much like static member
+--  variables in C++)
 --]]
 function class:__call(...)
     local cls = class_create()
@@ -76,11 +170,10 @@ function class:__call(...)
 
     function class_name(...)
         if select("#", ...) == 1 and type(...) == "string" then
-            cls.__name = ...
+            cls.__classname = ...
             return class_inherits
         else
-            -- TODO
-            cls.__name = "<unknown>"
+            class_try_get_classname(cls)
         end
         return class_inherits(...)
     end
@@ -100,6 +193,9 @@ function class:__call(...)
         if select("#", ...) > 0 or type(init) ~= "table" then
             error("syntax error")
         end
+        -- TODO
+        cls.__mro = {cls, default_base}
+        cls.__name = class.type(cls)
         for k, v in pairs(init) do cls[k] = v end
         return cls
     end
@@ -108,14 +204,11 @@ function class:__call(...)
 end
 
 function class.isinstance(instance, class)
-    local function recurse_isinstance(class_instance, class)
-        if class_instance == class then return true end
-        for _, base in ipairs(class_instance.__bases) do
-            if recurse_isinstance(base, class) then return true end
-        end
-        return false
+    local cls = instance.__class
+    for _, c in ipairs(cls.__mro) do
+        if c == class then return true end
     end
-    return recurse_isinstance(instance.__class, class)
+    return false
 end
 
 function class.type(obj)
@@ -129,13 +222,33 @@ function class.type(obj)
     else
         return nil
     end
-    return s .. "class " .. obj.__name
+    return s .. "class " .. cls.__classname
 end
 
 function class.isclass(cls)
     local bases = rawget(cls, '__bases')
     return bases and type(bases) == "table"
 end
+
+default_base.__classname = "object"
+default_base.__name = class.type(default_base)
+
+--[[!
+--  @brief Resolve an attribute in an instance or class
+--
+--  @param inst_or_class instance or class
+--  @param attr          attribute to find
+--
+--  @return nil if the attribute is not found, otherwise the attribute, the class where it is found,
+--          and a resolver. The resolver is simply a function that returns the next attributes and
+--          corresponding class that are found each time it is called and return nil when there is
+--          no more attribute to be found
+--
+--  @see class_resolve_attr
+--]]
+class.getattr = class_resolve_attr
+
+----------------------------------------------------------------------------------------------------
 
 if rawget(_G, "RUN_TESTS") then
     local Foo = class "Foo"
@@ -146,10 +259,17 @@ if rawget(_G, "RUN_TESTS") then
         end;
     }
 
+    io.stdout:setvbuf "no"
     f = Foo(42)
     print(f.bar)
     print(class.type(f))
     print(class.type(Foo))
+    print(f)
+    print(Foo)
+    print(class{})
+    local Bar = class {}
+    print(Bar)
+    print(Bar.__mro)
 end
 
 return class
